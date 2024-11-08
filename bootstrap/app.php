814bbf24804254041126511410154082d6929fca
Kim Jangwook <kim.jangwook@connecty.co.jp>
2024-11-08 16:34:46 +0900

    web middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->web([IpRestrictionMiddleware::class]);
         $middleware->append(IpRestrictionMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
