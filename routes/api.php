b1616d7aa88bd1a2a6796f14bfd4c45c327e6985
Kim Jangwook <kim.jangwook@connecty.co.jp>
2024-11-20 16:48:56 +0900

    cors

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,7 @@ use App\Http\Controllers\JobCanNotifyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/jobcan-notify', [JobCanNotifyController::class, 'index']);
-Route::post('/jobcan-notify/forms', [JobCanNotifyController::class, 'forms']);
+Route::middleware('cors')->group(function () {
+    Route::post('/jobcan-notify', [JobCanNotifyController::class, 'index']);
+    Route::post('/jobcan-notify/forms', [JobCanNotifyController::class, 'forms']);
+});
