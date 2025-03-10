e94659d99b18a42c1c0c371359018b3cc7802a97
Kim Jangwook <kim.jangwook@connecty.co.jp>
2025-03-10 15:09:55 +0900

    modify webhook url

--- config/env.php
+++ config/env.php
@@ -20,29 +20,29 @@ $jobcanContractForm = [
 
 return [
     'jobcan_api_key' => env('JOBCAN_API_KEY'),
-    'jobcan_estimate_slack_url' => 'https://hooks.slack.com/services/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',
+    'jobcan_estimate_slack_url' => 'https://hooks.slack.com/services/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',
     'jobcan_estimate_form' => $jobcanEstimateForm,
-    'jobcan_cost_slack_url' => 'https://hooks.slack.com/services/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',
+    'jobcan_cost_slack_url' => 'https://hooks.slack.com/services/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',
     'jobcan_cost_form' => $jobcanCostForm,
-    'jobcan_contract_slack_url' => 'https://hooks.slack.com/services/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',
+    'jobcan_contract_slack_url' => 'https://hooks.slack.com/services/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',
     'jobcan_contract_form' => $jobcanContractForm,
     'jobcan_form' => [
         ...$jobcanEstimateForm,
         ...$jobcanCostForm,
         ...$jobcanContractForm,
     ],
-    'slack_webhook_url' => 'https://hooks.slack.com/services/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',
-    'slack_webhook_for_test' => 'https://hooks.slack.com/services/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',
+    // 'slack_webhook_url' => 'https://hooks.slack.com/services/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',
+    'slack_webhook_for_test' => 'https://hooks.slack.com/services/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',
 
     'admin_key' => env('ADMIN_KEY'),
     'notion_api_key' => env('NOTION_API_KEY'),
 
     'company_info_notion_database_id' => '1056a5225659800f9b85eaece05d1853',
 
-    'ssl_webhook_url' => 'https://hooks.slack.com/services/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',
+    'ssl_webhook_url' => 'https://hooks.slack.com/services/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',
     'ssl_info_notion_database_id' => '15e6a522565980fba316ea78bc544d74',
 
-    'domain_webhook_url' => 'https://hooks.slack.com/services/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',
+    'domain_webhook_url' => 'https://hooks.slack.com/services/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',
     'domain_info_notion_database_id' => '1666a522565980d5aabee321bd11110f',
 
     'cms_ssl_info_notion_database_id' => '1666a522565980efb0c8cc9e7e1b913f',
