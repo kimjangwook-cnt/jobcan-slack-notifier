fdcdb0747ae0325478a62b754c712bee70741f80
Kim Jangwook <kim.jangwook@connecty.co.jp>
2025-03-26 10:16:19 +0900

    add kim's dedicated ip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->validateCsrfTokens(except: [
+            'csrf-token',
+        ]);
+
         $middleware->append(IpRestrictionMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
